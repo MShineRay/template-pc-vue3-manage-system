5274a4ea62a7e47a6857214733946c34c0c2e6c1
msr <mshineray@qq.com>
2022-06-23 16:22:45 +0800

    upd: clean

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -43,56 +43,56 @@ export default {
                 index: "/dashboard",
                 title: "系统首页",
             },
-            {
-                icon: "el-icon-lx-cascades",
-                index: "/table",
-                title: "基础表格",
-            },
-            {
-                icon: "el-icon-lx-copy",
-                index: "/tabs",
-                title: "tab选项卡",
-            },
-            {
-                icon: "el-icon-lx-calendar",
-                index: "3",
-                title: "表单相关",
-                subs: [
-                    {
-                        index: "/form",
-                        title: "基本表单",
-                    },
-                    {
-                        index: "/upload",
-                        title: "文件上传",
-                    },
-                    {
-                        index: "4",
-                        title: "三级菜单",
-                        subs: [
-                            {
-                                index: "/editor",
-                                title: "富文本编辑器",
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                icon: "el-icon-lx-emoji",
-                index: "/icon",
-                title: "自定义图标",
-            },
-            {
-                icon: "el-icon-pie-chart",
-                index: "/charts",
-                title: "schart图表",
-            },
-            {
-                icon: "el-icon-lx-global",
-                index: "/i18n",
-                title: "国际化功能",
-            },
+            // {
+            //     icon: "el-icon-lx-cascades",
+            //     index: "/table",
+            //     title: "基础表格",
+            // },
+            // {
+            //     icon: "el-icon-lx-copy",
+            //     index: "/tabs",
+            //     title: "tab选项卡",
+            // },
+            // {
+            //     icon: "el-icon-lx-calendar",
+            //     index: "3",
+            //     title: "表单相关",
+            //     subs: [
+            //         {
+            //             index: "/form",
+            //             title: "基本表单",
+            //         },
+            //         {
+            //             index: "/upload",
+            //             title: "文件上传",
+            //         },
+            //         {
+            //             index: "4",
+            //             title: "三级菜单",
+            //             subs: [
+            //                 {
+            //                     index: "/editor",
+            //                     title: "富文本编辑器",
+            //                 },
+            //             ],
+            //         },
+            //     ],
+            // },
+            // {
+            //     icon: "el-icon-lx-emoji",
+            //     index: "/icon",
+            //     title: "自定义图标",
+            // },
+            // {
+            //     icon: "el-icon-pie-chart",
+            //     index: "/charts",
+            //     title: "schart图表",
+            // },
+            // {
+            //     icon: "el-icon-lx-global",
+            //     index: "/i18n",
+            //     title: "国际化功能",
+            // },
             {
                 icon: "el-icon-lx-warn",
                 index: "7",
@@ -108,11 +108,11 @@ export default {
                     },
                 ],
             },
-            {
-                icon: "el-icon-lx-redpacket_fill",
-                index: "/donate",
-                title: "支持作者",
-            },
+            // {
+            //     icon: "el-icon-lx-redpacket_fill",
+            //     index: "/donate",
+            //     title: "支持作者",
+            // },
         ];
 
         const route = useRoute();
